55173da9b823bf01ff05a5a534003699546d2a30
unknown <aditiv@aditiv.cybage.com>
2016-09-19 18:30:26 +0530

    WebService
    Signed-off-by: unknown <aditiv@aditiv.cybage.com>

--- API/api.php
+++ API/api.php
@@ -4,28 +4,54 @@ $servername='localhost';
 $username = 'root';
 $password='';
 $dbname = 'employee';
-$table = 'emp_detail';
+//$table = 'emp_details';
 
 $link = mysqli_connect($servername,$username,$password,$dbname);
+
 
 
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
 	case 'GET':
-		$sql = "select* from `$table` ORDER by emp_id ASC";
+		$sql = "select* from emp_detail ORDER by emp_id DESC ";
 		break;
 
 	case 'POST':
-		$data = json_decode(file_get_contents("php://input"));
-		$emp_id = mysqli_real_escape_string($link,$data->emp_id);
-		$fname = mysqli_real_escape_string($link,$data->fname);
-		$mname = mysqli_real_escape_string($link,$data->mname);
-		$lname = mysqli_real_escape_string($link,$data->lname);
-		$city = mysqli_real_escape_string($link,$data->city);
-		$salary = mysqli_real_escape_string($link,$data->salary);
+		$data = file_get_contents("php://input");
 
-		$sql = "INSERT into `$table` (emp_id,fname,mname,lname,city,salary) VALUES ('$emp_id','$fname','$mname','$lname','$city','$salary')";
+		echo $data;
+		$data=json_decode($data);
+		/*$emp_id = mysqli_real_escape_string($link,$_POST['empID']);
+		$fname = mysqli_real_escape_string($link,$_POST['FName']);
+		$mname = mysqli_real_escape_string($link,$_POST['MName']);
+		$lname = mysqli_real_escape_string($link,$_POST['LName']);
+		$city = mysqli_real_escape_string($link,$_POST['City']);
+		$salary = mysqli_real_escape_string($link,$_POST['Salary']);
+		*/
+		/*$input=explode('&',$data);
+		print_r($input);
+		$i=0;
+		$value=[];
+		foreach ($input as $v) {
+			$value[$i++]=explode('=',$v);
+		}
+		print_r($value);
+		$sql = "INSERT into emp_detail (emp_id,fname,mname,lname,city,salary) VALUES (".$value[0][1].",'".$value[1][1]."','".$value[2][1]."','".$value[3][1]."','".$value[4][1]."',".$value[5][1].")";
+		echo $sql;*/
+
+			$sql = "INSERT into emp_detail (emp_id,fname,mname,lname,city,salary) VALUES (".$data->emp_id.",'".$data->fname."','".$data->mname."','".$data->lname."','".$data->city."',".$data->salary.")";
+		break;
+
+	case 'DELETE':
+		echo 'In delete case';
+		$data=$_GET['emp_id'];
+		echo $data;
+		//$data = json_decode(file_get_contents("php://input"));
+		//$emp_id = mysqli_real_escape_string($link,$data.emp_id);
+		//$value=explode('=',$data);
+		$sql = "DELETE FROM emp_detail WHERE emp_id=".$data;
+		//'".$emp_id."'"
 		break;
 	
 	default:
@@ -57,6 +83,14 @@ if($method == 'GET')
 if($method == 'POST')
 {
 	echo true;
+	//json_encode("");
+	//header('Location:index.html');
+}
+
+if($method == 'DELETE')
+{
+	echo true;
+	//header('Location:index.html');
 }
 
 ?>
